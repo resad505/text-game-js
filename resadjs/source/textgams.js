e9afd58b7017b12a4714ba57b49e0ea81f857cf1
unknown <resadrules8@gmail.com>
2025-12-13 15:58:35 +0400

    Initial commit - character stats setup

---
+++ resadjs/source/textgams.js
@@ -0,0 +1,4 @@
+/*hello everyone,this is a text game enjoy it !*/
+console.log("hello everyone,this is a text game enjoy it !");
+const readlineSync = require('readline-sync');
+const playername=readlineSync.question("Enter your name : ");
